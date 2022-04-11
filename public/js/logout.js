70aba42d64150eaaccba0683bc55dbfeab744e66
IverMB <bertel.iversen@hotmail.com>
2022-04-11 12:26:04 +0200

    dsa

--- public/js/logout.js
+++ public/js/logout.js
@@ -3,7 +3,7 @@ function GetCookie(authorization){
     for(let i = 0; i < CookieArray.length; i++) {
         const cookie = CookieArray[i]
         if (cookie.indexOf(authorization) != -1) {
-            document.cookie = "authorization = value; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            document.cookie = "authorization = value; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
         }
     }
     window.location.pathname = './'
