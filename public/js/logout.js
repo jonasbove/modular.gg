bf7d780297c8e3b2908d1a2a18b6be155a70c853
Sebastian <seb@livoni.me>
2022-04-11 12:27:32 +0200

    deleted unnescesarry code

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,8 @@
-function GetCookie(authorization){
-    let CookieArray = document.cookie.split(";");
-    for(let i = 0; i < CookieArray.length; i++) {
-        const cookie = CookieArray[i]
-        if (cookie.indexOf(authorization) != -1) {
-            document.cookie = "authorization=value; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-        }
-    }
-    window.location.pathname = './'
+function DeleteCooKie(authorization){
+    document.cookie = `${authorization}=value; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+    window.location.href = './'
 }
 
 document.querySelector("#logout").addEventListener("click", function(e) {
-    GetCookie("authorization")
+    DeleteCooKie("authorization")
 })
