07fb1c067a388975be385d714573430e7ce4d167
IverMB <bertel.iversen@hotmail.com>
2022-04-11 11:44:00 +0200

    created logout

---
+++ public/js/logout.js
@@ -0,0 +1,14 @@
+function GetCookie(authorization){
+    let CookieArray = document.cookie.split(";");
+    for(let i = 0; i < CookieArray.length; i++) {
+        const cookie = CookieArray[i]
+        if (cookie.indexOf(authorization) != -1) {
+            document.cookie = `${authorization}=value; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+        }
+    }
+    //window.location.pathname = '/'
+}
+
+document.querySelector("#logout").addEventListener("click", function(e) {
+    GetCookie("authorization")
+})
