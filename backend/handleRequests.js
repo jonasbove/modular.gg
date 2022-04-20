d628cb9cc67b77ee6d91b8009a69709aeae5df3e
Sebastian Livoni <seb@livoni.me>
2022-04-20 14:23:57 +0200

    fixed js

--- backend/handleRequests.js
+++ backend/handleRequests.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const compile = require('./compiler/compileJSON.js')
-import dotenv from 'dotenv'
+const dotenv = require('dotenv')
 
 dotenv.config({ path: '../.env' })
 
