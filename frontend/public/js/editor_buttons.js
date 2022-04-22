68f3bbd00dbdcee419ce213b91e488464620a10c
Sebastian Livoni <seb@Sebs-Macbook-Pro-Intel.local>
2022-04-22 21:55:29 +0200

    added sidebar and topbar to editor

---
+++ frontend/public/js/editor_buttons.js
@@ -0,0 +1,33 @@
+const events = document.querySelectorAll('.events a')
+
+events.forEach(anchor => {
+  anchor.addEventListener('click', event => {
+    event.preventDefault()
+
+    switch(anchor.getAttribute('event')) {
+      case 'onCommand':
+        e.spawnNode(new VPL_Node("OnSlashCommand", [new ActionPlug("next")], [new InPlug(GraphType.Text, "trigger", true)], [new OutPlug(GraphType.Channel, "channel"), new OutPlug(GraphType.Text, "text")], new point(250, 175), 0, true))
+        break;
+    }
+  })
+})
+
+const effects = document.querySelectorAll('.effects a')
+
+effects.forEach(anchor => {
+  anchor.addEventListener('click', event => {
+    event.preventDefault()
+
+    switch(anchor.getAttribute('effect')) {
+      case 'ifelse':
+        e.spawnNode(new ActionNode("IfElse", [new ActionPlug("if"), new ActionPlug("else")], [new InPlug(GraphType.Bool, "expression", false)], [], new point(250, 175), 193, false))
+        break;
+      case 'sendMessage':
+        e.spawnNode(new ActionNode("SendMessage", [], [new InPlug(GraphType.Channel, "channel", false), new InPlug(GraphType.Text, "text", true)], [], new point(250, 175), 494, false))
+        break;
+      case 'greaterThan':
+        e.spawnNode(new VPL_Node("GreaterThan", [], [new InPlug(GraphType.Num, "a", true), new InPlug(GraphType.Num, "b", true)], [new OutPlug(GraphType.Bool, "result")], new point(250, 175), 190, false))
+        break;
+    }
+  })
+})
