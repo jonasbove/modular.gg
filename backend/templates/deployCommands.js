9940fa9a5d5ddec1140aee5aae58de0774cb262b
seb <29739749+sebastianlivoni@users.noreply.github.com>
2022-04-28 11:16:29 +0200

    Update deployCommands.js

--- backend/templates/deployCommands.js
+++ backend/templates/deployCommands.js
@@ -8,7 +8,7 @@ dotenv.config({ path: '../../.env' })
 
 export default function deployCommands(commands, token) {
   const rest = new REST({ version: '9' }).setToken(token);
-  console.log("Commands to deploy: " + commands)
+  console.log("Commands to deploy: " + JSON.stringify(commands))
 
 	return rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
 		.then(() => console.log('Successfully registered application commands.'))
