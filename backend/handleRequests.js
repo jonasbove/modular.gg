e7cb2be506185ce1d9e20f75f639f3ee3df2b5ef
seb <29739749+sebastianlivoni@users.noreply.github.com>
2022-04-28 11:38:47 +0200

    Update handleRequests.js

--- backend/handleRequests.js
+++ backend/handleRequests.js
@@ -20,6 +20,7 @@ let botMan = new botManager()
 app.post('/addJSON', async (req, res) => {
   console.log("Got json request")
   const userData = await verifyToken(req)
+  console.log(userData)
   const botToken = userData.discordBotToken
 
   //return console.log(botToken)
